c695ccd74ad29089eb0d304e8815043c213f21fc
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-06-15 15:26:50 -0400

    changes

--- main.js
+++ main.js
@@ -25,10 +25,25 @@ return 'That\'s a small string!'
 function howLongIsMyString(size) {
   if(size.length > 20) {
   return 'That\'s a long string!'
-  }else if(size.length <= 10 && 20<=){
-    return 'That\'s a regular sized string!'
-  }
+  }else if (size.length >= 10){
+return 'That\'s a regular sized string!'
+}else {
+  return 'That\'s a small string!'
 }
+}
+function instructorHeight(str){
+if (str === 'Colin'){
+  return 62;
+}else if (str === 'Mesuara'){
+return 69;
+}else{
+  return 'I don\'t know that instructor!'
+}
+}
+
+
+
+
 
   
   
